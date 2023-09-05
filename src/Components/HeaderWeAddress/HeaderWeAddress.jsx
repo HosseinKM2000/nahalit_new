8811920af5a8e7cf2eab5ca1fa53036243e01ecb
Hossein_km <hosseinkamari909908@gmail.com>
2023-09-05 10:58:46 +0330

    change address in header to component

---
+++ src/Components/HeaderWeAddress/HeaderWeAddress.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function HeaderWeAddress() {
+  return (
+    <div className="flex justify-between w-full text-center px-5 py-1">
+        <p className="font-[shabnamLight]  leading-10 text-[#707070] text-[0.9rem]">
+        آدرس : تهران میدان فردوسی خیابان ایرانشهر بین سمیه و طالقانی مجتمع تجاری میلاد واحد 9
+        </p>
+        <p className="font-[shabnamLight] leading-10 text-[#707070] text-[0.9rem]">پشتیبانی 24 ساعته : 02188867940 – 09927674217</p>
+    </div>
+  )
+}
+
+export default HeaderWeAddress

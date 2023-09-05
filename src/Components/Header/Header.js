d64519dcfa3bc923aef791bf0513308cf914156f
Hossein_km <hosseinkamari909908@gmail.com>
2023-09-05 11:40:31 +0330

    change HeaderNavButtons to component

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,21 +1,17 @@
 import Cookies from "js-cookie";
 import React, { useEffect, useState } from "react";
-import { AiFillDashboard, AiOutlineHeart } from "react-icons/ai";
-import { BsPerson } from "react-icons/bs";
-import { FaSpa, FaUserCircle } from "react-icons/fa";
+import { AiOutlineHeart } from "react-icons/ai";
 import { HiOutlineShoppingCart } from "react-icons/hi";
-import { MdKeyboardArrowLeft } from "react-icons/md";
-import { RiArrowDownSLine } from "react-icons/ri";
 import { useDispatch, useSelector } from "react-redux";
 import { Link, useLocation } from "react-router-dom";
 import { getBasketsByUserId } from "../../features/cart/action";
 import { getUserRole } from "../../features/dashboard/action";
+import HeaderAdminProfileIcon from "../HeaderAdminProfileIcon/HeaderAdminProfileIcon";
+import HeaderNavButtons from "../HeaderNavButtons/HeaderNavButtons";
+import HeaderNavbar from "../HeaderNavbar/HeaderNavbar";
+import HeaderProductsLogo from "../HeaderProductsLogo/HeaderProductsLogo";
 import HeaderWeAddress from "../HeaderWeAddress/HeaderWeAddress";
 import ScrollTop from "../ScrollTop/ScrollTop";
-import UserAvatarIcon from "../UserAvatar/UserAvatar";
-import HeaderProductsLogo from "../HeaderProductsLogo/HeaderProductsLogo";
-import HeaderAdminProfileIcon from "../HeaderAdminProfileIcon/HeaderAdminProfileIcon";
-import HeaderNavbar from "../HeaderNavbar/HeaderNavbar";
 
 const Header = () => {
   const [showSlide4, setShowSlide4] = useState(false);
@@ -64,48 +60,10 @@ const Header = () => {
         <HeaderWeAddress/>
         <HeaderProductsLogo/>
         <div className="flex justify-center mt-3 items-center font-[shabnamMedium] py-5" style={{borderBottom:'1px #D6D3D1 solid',borderTop:'1px #D6D3D1 solid'}}>
-          <div className="text-[#7c7c7c] text-sm flex items-center justify-center w-full gap-5 2xl:justify-center 2xl:gap-x-[8rem] px-3">
-
-              <HeaderAdminProfileIcon showSlide13={showSlide13} setShowSlide13={setShowSlide13} userId={userId} loginStatus={loginStatus}/>
+        <div className="text-[#7c7c7c] text-sm flex items-center justify-center w-full gap-5 2xl:justify-center 2xl:gap-x-[8rem] px-3">
+            <HeaderAdminProfileIcon showSlide13={showSlide13} setShowSlide13={setShowSlide13} userId={userId} loginStatus={loginStatus}/>
             <HeaderNavbar/>
-            <div className="flex items-center justify-center xl:gap-x-3 lg:gap-x-3">
-              <div className="bg-[#57C053] min-w-fit hover:bg-[#62d15e] transition-all duration-300 xl:px-3 text-white flex flex-row-reverse px-1 py-2 rounded-md items-center justify-center gap-x-2" style={{boxShadow:'rgba(99, 99, 99, 0.2) 0px 2px 8px 0px'}}>
-                <Link to={"/be_seller"}>
-                  <span className="text-sm font-[shabnamMedium] tracking-tight">فروشنده باشید</span>
-                </Link>
-                <FaSpa className="text-base" />
-              </div>
-              {
-                loginStatus
-                ?
-                <></>
-                :
-                <Link to={'/login'} className="flex flex-row-reverse items-center justify-center py-2 px-1 xl:px-3 rounded-md bg-[#57C053] hover:bg-[#62d15e] text-white gap-x-2" style={{boxShadow:'rgba(99, 99, 99, 0.2) 0px 2px 8px 0px'}}>
-                  <span className="text-sm font-[shabnamMedium] tracking-tight">ورود و عضویت</span>
-                  <BsPerson className="text-base" />
-              </Link>
-              }
-              {
-                userId !== '' && userId === 1
-                ?
-                <Link to={'/dashboard'} className="flex flex-row-reverse w-[7rem] min-w-fit items-center justify-center py-2 px-1 xl:px-3 rounded-md bg-[#57C053] hover:bg-[#62d15e] text-white gap-x-2" style={{boxShadow:'rgba(99, 99, 99, 0.2) 0px 2px 8px 0px'}}>
-                  <span className="text-sm font-[shabnamMedium] tracking-tight">پنل مدیریت</span>
-                  <AiFillDashboard className="text-lg" />
-                </Link>
-              :
-              <></>
-              }
-              {
-                userId !== '' && userId !== 1
-                ?
-                <Link to={'/userPage/userProfile'} className="flex flex-row-reverse w-[7rem] min-w-fit items-center justify-center py-2 px-1 xl:px-3 rounded-md bg-[#57C053] hover:bg-[#62d15e] text-white gap-x-2" style={{boxShadow:'rgba(99, 99, 99, 0.2) 0px 2px 8px 0px'}}>
-                  <span className="text-sm font-[shabnamMedium] tracking-tight">پنل کاربری</span>
-                  <FaUserCircle className="text-lg" />
-                </Link>
-              :
-              <></>
-              }
-            </div>
+            <HeaderNavButtons loginStatus={loginStatus} userId={userId}/>
             <div className="flex items-center justify-center flex-row gap-2">
               {
                 urlPath === '/favorites'
@@ -143,7 +101,7 @@ const Header = () => {
                 </div>
               </div>
             </div>
-          </div>
+        </div>
         </div>
       </div>
     </>
---
+++ src/Components/HeaderNavButtons/HeaderNavButtons.jsx
@@ -0,0 +1,48 @@
+import React from 'react';
+import { AiFillDashboard } from "react-icons/ai";
+import { BsPerson } from "react-icons/bs";
+import { FaSpa, FaUserCircle } from "react-icons/fa";
+import { Link } from 'react-router-dom';
+
+function HeaderNavButtons({ loginStatus , userId }) {
+  return (
+    <div className="flex items-center justify-center xl:gap-x-3 lg:gap-x-3">
+        <Link to={"/be_seller"} className="flex flex-row-reverse items-center justify-center py-2 px-1 xl:px-3 rounded-md bg-[#57C053] hover:bg-[#62d15e] text-white gap-x-2" style={{boxShadow:'rgba(99, 99, 99, 0.2) 0px 2px 8px 0px'}}>
+        <span className="text-sm font-[shabnamMedium] tracking-tight">فروشنده باشید</span>
+        <FaSpa className="text-base" />
+        </Link>
+        {
+        loginStatus
+        ?
+        <></>
+        :
+        <Link to={'/login'} className="flex flex-row-reverse items-center justify-center py-2 px-1 xl:px-3 rounded-md bg-[#57C053] hover:bg-[#62d15e] text-white gap-x-2" style={{boxShadow:'rgba(99, 99, 99, 0.2) 0px 2px 8px 0px'}}>
+        <span className="text-sm font-[shabnamMedium] tracking-tight">ورود و عضویت</span>
+        <BsPerson className="text-base" />
+        </Link>
+        }
+        {
+        userId !== '' && userId === 1
+        ?
+        <Link to={'/dashboard'} className="flex flex-row-reverse w-[7rem] min-w-fit items-center justify-center py-2 px-1 xl:px-3 rounded-md bg-[#57C053] hover:bg-[#62d15e] text-white gap-x-2" style={{boxShadow:'rgba(99, 99, 99, 0.2) 0px 2px 8px 0px'}}>
+        <span className="text-sm font-[shabnamMedium] tracking-tight">پنل مدیریت</span>
+        <AiFillDashboard className="text-lg" />
+        </Link>
+        :
+        <></>
+        }
+        {
+        userId !== '' && userId !== 1
+        ?
+        <Link to={'/userPage/userProfile'} className="flex flex-row-reverse w-[7rem] min-w-fit items-center justify-center py-2 px-1 xl:px-3 rounded-md bg-[#57C053] hover:bg-[#62d15e] text-white gap-x-2" style={{boxShadow:'rgba(99, 99, 99, 0.2) 0px 2px 8px 0px'}}>
+        <span className="text-sm font-[shabnamMedium] tracking-tight">پنل کاربری</span>
+        <FaUserCircle className="text-lg" />
+        </Link>
+        :
+        <></>
+        }
+    </div>
+  )
+}
+
+export default HeaderNavButtons
